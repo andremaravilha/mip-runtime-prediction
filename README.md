a0b39da664600046dac269d41afebd4c8304d29d
André L. Maravilha <andre.maravilha@outlook.com>
2023-10-24 18:48:37 -0300

    Add code to repository

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# mip-runtime-prediction
-MIP runtime prediction.
+# Prediction of computational runtime for solving MIP problems using Gurobi
+
+> **Contributors:** Yuri Dimitre Dias de Faria<sup>1</sup>, André L. Maravilha<sup>2</sup>  
+> <sup>1</sup> *Undergraduate in Computer Engineering - Centro Fed. de Edu. Tecnológica de Minas Gerais ([url](https://www.cefetmg.br/))*  
+> <sup>2</sup> *Dept. of Informatics, Management and Design - Centro Fed. de Edu. Tecnológica de Minas Gerais ([url](https://www.cefetmg.br/))*  
+
+
+## 1. About
+
+Repository with the source code and the results of computational experiments used to build a runtime predictor for solving MIP problems using Gurobi solver.
+
+
+## 2. How to run this project
+
+Some important comments before building the project:
+
+* This project was developed with Python 3.11 and it uses `gurobipy` (v. 10.0.3), a Gurobi's API for Python. However, no license for Gurobi solver is provided with the code in this repository. Visit Gurobi website ([https://www.gurobi.com/](https://www.gurobi.com/)) to find out how to obtain a license.
+* Command in sections below assumes your Python executable is `python` and the Package Installer for Python (pip) is `pip`.
+* Besides, it assumes the `venv` module is installed, since it will be used to build the Python Virtual Environment to run the project.
+
+#### 2.2. Create and activate a Python Virtual Environment (venv)
+
+First, you need to clone this repository or download it to your machine. Then, inside the root directory of the project, create a Python Virtual Environment (venv):
+```
+python -m venv ./venv
+```
+
+After that, you need to activate the virtual environment (venv).
+
+In Linux machines, it is usually achieved by running the following command: 
+```
+source ./venv/bin/activate
+```
+
+On Windows:
+```
+.\venv\Scripts\activate
+```
+
+If you want to leave the virtual environment, run:
+```
+deactivate
+```
+
+#### 2.3. Installing dependencies
+
+With your virtual environment installed, you need to install the dependencies required by this project: 
+```
+pip install -r requirements.txt
+```
+
+#### 2.4. Installing dependencies
+
+Now you are ready to run the code! For this, just run the Python script `main.py`:
+```
+python main.py
+```
+
+This script will download the selected MIPLIB 2017 ([https://miplib.zib.de/](https://miplib.zib.de/)) instances to `data/instances` folder and solve them with Gurobi solver. After it is finished, the results will be available in `data/results.csv` file. Besides, Gorobi logs for each instance will be in `data/gurobi_log` folder.
---
+++ main.py
@@ -0,0 +1,199 @@
+import os
+import datetime
+import json
+import urllib.request
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import RLock
+import gurobipy as gp
+from gurobipy import GRB
+
+
+# Some global objects
+global lock
+lock = RLock()
+
+
+# MIPLIB2017 instance filter
+def filter_miplib2017(instance):
+
+    # Only easy instances are considered
+    if instance['status'] in ['open', 'hard']:
+        return False
+
+    # Only instances with a known optimal solution are considered
+    if instance['is_infeasible'] or instance['is_unbounded'] or not instance['is_optimal']:
+        return False
+    
+    # Only MIP instances are considered
+    if instance['size']['binaries']['original'] + instance['size']['integers']['original'] < 1:
+        return False
+    
+    # tags:
+    # 'aggregations', 'benchmark', 'benchmark_suitable', 'binary', 
+    # 'binpacking', 'cardinality', 'decomposition', 'equation_knapsack', 
+    # 'feasibility', 'general_linear', 'indicator', 'infeasible', 
+    # 'integer_knapsack', 'invariant_knapsack', 'knapsack', 'mixed_binary', 
+    # 'no_solution', 'numerics', 'precedence', 'set_covering', 'set_packing', 
+    # 'set_partitioning', 'variable_bound'
+    forbidden_tags = {'decomposition', 'feasibility', 'indicator', 'infeasible', 'no_solution', 'numerics'}
+    if instance['tags'] is not None and len(instance['tags']) > 0:
+        for tag in instance['tags']:
+            if tag in forbidden_tags:
+                return False
+    
+    return True
+
+
+# Auxiliary function to get a value or its default
+def get_value(o, keys, default):
+    success = True
+    for k in keys:
+        if o is not None:
+            o = o.get(k, None)
+        else:
+            success = False
+            break
+    return o if success else default
+
+
+# Get instance features
+def get_instance_features(instance):
+    return map(lambda x: str(x), 
+               [instance['objective'],
+                instance['size']['variables']['original'],
+                instance['size']['binaries']['original'],
+                instance['size']['integers']['original'],
+                instance['size']['continuous']['original'],
+                instance['size']['constraints']['original'],
+                instance['size']['nonzero_density']['original'],
+                get_value(instance, ['constraints', 'empty', 'original'], 0),
+                get_value(instance, ['constraints', 'free', 'original'], 0),
+                get_value(instance, ['constraints', 'singleton', 'original'], 0),
+                get_value(instance, ['constraints', 'aggregations', 'original'], 0),
+                get_value(instance, ['constraints', 'precedence', 'original'], 0),
+                get_value(instance, ['constraints', 'variable_bound', 'original'], 0),
+                get_value(instance, ['constraints', 'set_partitioning', 'original'], 0),
+                get_value(instance, ['constraints', 'set_packing', 'original'], 0),
+                get_value(instance, ['constraints', 'set_covering', 'original'], 0),
+                get_value(instance, ['constraints', 'bin_packing', 'original'], 0),
+                get_value(instance, ['constraints', 'knapsack', 'original'], 0),
+                get_value(instance, ['constraints', 'integer_knapsack', 'original'], 0),
+                get_value(instance, ['constraints', 'cardinality', 'original'], 0),
+                get_value(instance, ['constraints', 'invariant_knapsack', 'original'], 0),
+                get_value(instance, ['constraints', 'equation_knapsack', 'original'], 0),
+                get_value(instance, ['constraints', 'mixed_binary', 'original'], 0),
+                get_value(instance, ['constraints', 'general_linear', 'original'], 0)])
+
+
+# Function to run Gurobi solver in a subprocess
+def run_gurobi_solver(instance, seed, output_file):
+    global lock
+    status = 'unknown'
+    runtime = ''
+    nodes = ''
+    objective = ''
+
+    try:
+
+        # Download the instance
+        #filename = os.path.join('data', 'instances', f'{instance["name"]}.mps.gz')
+        filename = os.path.join('data', 'instances', f'{instance["name"]}.mps')
+        if not os.path.exists(filename):
+            urllib.request.urlretrieve(instance['url_download'], filename=filename)
+
+        # Load the instance
+        gp.setParam('OutputFlag', 1)
+        gp.setParam('LogToConsole', 0)
+
+        model = gp.read(filename)
+        model.setParam('Threads', 1)
+        model.setParam('Seed', seed)
+        #model.setParam('TimeLimit', 3600)
+        model.setParam('LogFile', 
+                       os.path.join('data', 'gurobi_log', 
+                                      f'{instance["name"]}_{seed}_{datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")}.log'))
+
+        # Solve the instance
+        model.optimize()
+    
+        # Get the results
+        runtime = model.Runtime
+        nodes = model.NodeCount
+        if model.SolCount > 0:
+            objective = model.ObjVal
+            if model.Status == GRB.OPTIMAL:
+                status = 'optimal'
+            else:
+                status = 'feasible'
+        else:
+            if model.Status == GRB.INF_OR_UNBD:
+                status = 'infeasible_or_unbounded'
+            elif model.Status == GRB.INFEASIBLE:
+                status = 'infeasible'
+            elif model.Status == GRB.UNBOUNDED:
+                status = 'unbounded'
+
+    except Exception as e:
+        print(f'Error while solving instance [ instance: {instance["name"]}, seed: {seed} ]: {e}')
+        status = 'error'
+        runtime = ''
+        nodes = ''
+        objective = ''
+    
+    # Write the results to the output file
+    with lock:
+        try: 
+            with open(output_file, 'a') as f:
+                line = f'{instance["name"]},{seed},{status},{objective},{nodes},{runtime},' + ','.join(get_instance_features(instance)) + '\n'
+                f.write(line)
+        except Exception as e:
+            print(f'Error while writing results [ instance: {instance["name"]}, seed: {seed} ]: {e}')
+
+
+# Run the computational experiments
+def main():
+
+    workers = 4
+    output_file = os.path.join('data', 'results.csv')
+    seeds = [1, 3]
+    
+    # Load a list of selected instances from MIPLIB2017
+    instances = dict()
+    with open(os.path.join('data', 'miplib2017.json')) as f:
+        miplib = json.load(f)
+        for instance in miplib:
+            if filter_miplib2017(instance):
+                instances[instance['name']] = instance    
+
+    # Create and initialize the output file
+    if not os.path.exists(output_file):
+        with open(output_file, 'w') as f:
+            columns = ['instance', 'seed', 'opt.status', 'opt.obj', 'opt.nodes', 'opt.runtime', 
+                       'optimal', 'var.size', 'var.binaries', 'var.integers', 'var.continuous', 
+                       'constr.size', 'const.density', 'constr.empty', 'constr.free', 'constr.singleton',
+                       'constr.aggregation', 'constr.precedence', 'constr.variable_bound', 'constr.set_partitioning',
+                       'constr.set_packing', 'constr.set_covering', 'constr.binpacking', 'constr.knapsack', 
+                       'constr.integer_knapsack', 'constr.cardinality', 'constr.invariant_knapsack', 
+                       'constr.equation_knapsack', 'constr.mixed_binary', 'constr.general_linear']
+            f.write(','.join(columns) + '\n')
+    
+    # Check entries (i.e., pairs of instance and seed) already solved
+    solved_entries = []
+    with open(output_file, 'r') as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            columns = line.split(',')
+            if columns[2] not in ['unknown', 'error', '']:
+                solved_entries.append( (columns[0], columns[1]) )
+    
+    # Run unsolved entries (i.e., pairs of instance and seed)
+    with ProcessPoolExecutor(max_workers=workers) as executor:
+        for seed in seeds:
+            for key in instances.keys():
+                instance = instances[key]
+                if (instance['name'], seed) not in solved_entries:
+                    executor.submit(run_gurobi_solver, instance, seed, output_file)
+
+
+if __name__ == '__main__':
+    main()
